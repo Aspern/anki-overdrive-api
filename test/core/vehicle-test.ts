aaa157f25f44d003444613b6c41c2217b4289fb4
Adrian Weber <adrian@kuebler-weber.de>
2017-01-12 19:51:17 +0100

    added more tests

--- test/core/vehicle-test.ts
+++ test/core/vehicle-test.ts
@@ -2,7 +2,8 @@ import {suite, test, timeout} from "mocha-typescript";
 import {expect} from "chai";
 import {VehicleScanner} from "../../src/core/vehicle/vehicle-scanner";
 import {Vehicle} from "../../src/core/vehicle/vehicle-interface";
-
+import {message} from "gulp-typescript/release/utils";
+import {PositionUpdateMessage} from "../../src/core/message/position-update-message";
 
 @suite
 class VehicleTest {
@@ -20,8 +21,13 @@ class VehicleTest {
         });
     }
 
-    @test "hello World"() {
-        expect(true).to.be.true;
+    @test @timeout(5000) "connect and disconnect"(done: Function) {
+        VehicleTest._vehicle.connect().then(() => {
+            VehicleTest._vehicle
+                .disconnect()
+                .then(() => done())
+                .catch((e) => done(e));
+        }).catch((e) => done(e));
     }
 
 
