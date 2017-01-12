b58ac3b413b6a5f26532eebcf4ba43eb6f16fea0
Adrian Weber <adrian@kuebler-weber.de>
2017-01-12 19:05:10 +0100

    removed istanbul package

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,10 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
-var remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
+
 
 gulp.task("default", function () {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist"));
-});
-
-gulp.task('remap-istanbul', function () {
-    return gulp.src('test/coverage-final.json')
-        .pipe(remapIstanbul())
-        .pipe(gulp.dest('coverage-remapped.json'));
 });
