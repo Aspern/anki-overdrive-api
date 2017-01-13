a406fa14f221abebf752cbd4d09f1709cb103ad0
Adrian Weber <adrian@kuebler-weber.de>
2017-01-13 11:54:15 +0100

    Delete driving-direction.js

--- src/core/message/driving-direction.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var DrivingDirection;
-(function (DrivingDirection) {
-    DrivingDirection[DrivingDirection["FORWARD"] = 0] = "FORWARD";
-    DrivingDirection[DrivingDirection["REVERSE"] = 1] = "REVERSE";
-})(DrivingDirection || (DrivingDirection = {}));
-exports.DrivingDirection = DrivingDirection;
-//# sourceMappingURL=driving-direction.js.map
