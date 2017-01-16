da49bc5bd11d3f26fb315bada31ce5a652acceff
Adrian Weber <adrian@kuebler-weber.de>
2017-01-16 11:37:59 +0100

    added track-scanner to find locations for pieces

--- src/util/track-scanner.ts
+++ src/util/track-scanner.ts
@@ -4,10 +4,16 @@ import {PositionUpdateMessage} from "../core/message/position-update-message";
 
 let scanner = new VehicleScanner(),
     vehicleId: string = process.argv[2],
-    startPiece = 34;
+    startPiece = 34,
+    offset = -60.0;
 
 if (!vehicleId)
     throw new Error("Please send vehicle-id as 1st parameter.");
+
+function onError(e: Error) {
+    console.error(e);
+    process.exit(1);
+}
 
 function findStart(vehicle: Vehicle): Promise<Vehicle> {
     return new Promise<Vehicle>((resolve) => {
@@ -20,24 +26,33 @@ function findStart(vehicle: Vehicle): Promise<Vehicle> {
         };
         vehicle.addListener(listener, PositionUpdateMessage);
         vehicle.setSpeed(300, 250);
+        setTimeout(() => vehicle.changeLane(offset), 1000);
+        setTimeout(() => vehicle.changeLane(offset), 2000);
     });
 };
 
 function scanTrack(vehicle) {
-    var lastPiece;
+    var lastPiece,
+        locations = [];
 
     vehicle.addListener((msg: PositionUpdateMessage) => {
 
-        if (lastPiece && msg.piece === startPiece)
+        if (lastPiece && msg.piece === startPiece) {
             vehicle.setSpeed(0, 1500);
+            setTimeout(() => process.exit(0), 1000);
+
+        }
 
         if (!lastPiece)
             lastPiece = msg.piece;
 
         if (msg.piece !== lastPiece) {
-            console.log(lastPiece + ", ");
+            console.log("piece: " + lastPiece + " => locations: " + locations + ", offset: " + msg.offset + "mm");
             lastPiece = msg.piece;
+            locations = [];
         }
+
+        locations.push(msg.location);
 
     }, PositionUpdateMessage);
 
@@ -54,16 +69,12 @@ function measure(vehicle: Vehicle): void {
 function connect(vehicle: Vehicle): void {
     vehicle.connect()
         .then(measure)
-        .catch((e) => {
-            throw e;
-        });
+        .catch(onError);
 };
 
 scanner.findById(vehicleId)
     .then(connect)
-    .catch((e) => {
-        throw e;
-    });
+    .catch(onError);
 
 
 
