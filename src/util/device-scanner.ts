df6360464b4230d20d2053707dbdb07942d2e6c9
Adrian Weber <adrian@kuebler-weber.de>
2017-01-17 16:18:46 +0100

    added util "scan" and "measure-distance-lane".

--- src/util/device-scanner.ts
+++
@@ -1,5 +0,0 @@
-import {VehicleScanner} from "../core/vehicle/vehicle-scanner";
-
-let scanner = new VehicleScanner();
-
-scanner.findAll().then(console.log).catch(console.error);
---
+++ src/util/scan.ts
@@ -0,0 +1,12 @@
+import {VehicleScanner} from "../core/vehicle/vehicle-scanner";
+
+let scanner = new VehicleScanner(),
+    onError = (e: Error) => {
+        console.error(e);
+        process.exit(1);
+    };
+
+scanner.findAll().then((vehicles) => {
+    console.log(vehicles);
+    process.exit(0);
+}).catch(onError);
