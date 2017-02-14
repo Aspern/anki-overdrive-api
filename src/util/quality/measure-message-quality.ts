0137fa9c8ca98c453f1ed5ab59f1fd8bb67b2b51
Adrian Weber <adrian@kuebler-weber.de>
2017-02-14 17:58:48 +0100

    updated quality util.

--- src/util/quality/measure-message-quality.ts
+++ src/util/quality/measure-message-quality.ts
@@ -1,4 +1,11 @@
 import {VehicleScanner} from "../../core/vehicle/vehicle-scanner";
+import {Settings} from "../../settings/settings-interface";
+import {JsonSettings} from "../../settings/json-settings";
 
 let scanner = new VehicleScanner(),
-    tra
+    settings: Settings = new JsonSettings(),
+    track = settings.getAsTrack("track");
+
+track.eachPiece(piece => {
+    console.log(piece.id);
+});
