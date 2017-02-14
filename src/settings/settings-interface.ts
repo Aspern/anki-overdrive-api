7f40de1f4b6fb2dc3adfc06f164e21377c5f07a1
Adrian Weber <adrian@kuebler-weber.de>
2017-02-14 17:53:10 +0100

    added settings module with tests and examples.ü

---
+++ src/settings/settings-interface.ts
@@ -0,0 +1,21 @@
+import {Track} from "../core/track/track-interface";
+
+interface Settings {
+
+    get(key: string): string;
+
+    getAsString(key: string, defaultValue: string): string;
+
+    getAsInt(key: string, defaultValue: number): number;
+
+    getAsFloat(key: string, defaultValue: number): number;
+
+    getAsBoolean(key: string, defaultValue: boolean): boolean;
+
+    getAsDate(key: string, defaultValue: Date): Date;
+
+    getAsTrack(key: string): Track
+
+}
+
+export {Settings};
