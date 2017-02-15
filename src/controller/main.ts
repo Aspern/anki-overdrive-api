5fe0e64ec78f7f92505c5b94a7b6da6945832604
Waqar Ahmed <waqar.ahmed@rwth-aachen.de>
2017-02-15 17:16:09 +0100

    working on kafka test

--- src/controller/main.ts
+++ src/controller/main.ts
@@ -4,6 +4,7 @@ import {AnkiConsole} from "./console";
 import {PositionUpdateMessage} from "../core/message/position-update-message";
 import {KafkaController} from "./kafka/kafkacontroller";
 import {ConsumerMessage} from "./kafka/ConsumerMessage";
+import HLJSStatic = hljs.HLJSStatic;
 
 let vehicles: Array<Vehicle>;
 
@@ -12,39 +13,47 @@ console.log("scanning vehicles...");
 let scanner = new VehicleScanner();
 let ankiConsole = new AnkiConsole();
 
+let kafka = new KafkaController('localhost:2181');
+
+let isProducerStarted = false;
+
+kafka.addListener((message: any) => {
+    console.log(JSON.parse(message.value));
+
+}, ConsumerMessage);
+
+kafka.initializeConsumer([{ topic: 'test', partition: 0 }], 6);
+
+kafka.initializeProducer().then((isStarted: boolean)=> {
+    isProducerStarted = isStarted;
+    //isStarted ? kafka.sendPayload([ { topic: 'test', messages: "finally working again" , partitions: 1 }]): console.log('not started');
+});
+
+
 scanner.findAll().then((vehicles)=>{
     this.vehicles = vehicles;
     vehicles.forEach(function(vehicle, index) {
         console.log("use index " + index + " for car id: " + vehicle.id);
+        vehicle.addListener((message) => {
+            //console.log(message);
+            isProducerStarted ? kafka.sendPayload([ { topic: 'test', messages: message , partitions: 1 }]): console.log('not started');
+        }, PositionUpdateMessage);
     });
     ankiConsole.initializePrompt(vehicles);
-
-    vehicles[0].addListener((message) => {
-        console.log(message);
-    }, PositionUpdateMessage);
 });
 
-var propertiesFileName = 'config.properties';
+
+
+/*var propertiesFileName = 'config.properties';
 var properties = require ("properties");
-
-
 
 properties.parse(__dirname + '/../../' + propertiesFileName, {path: true}, function(error: any, obj: any) {
     if (error) return console.error (error);
     console.log (obj);
-});
+});*/
 
 
 
-let kafka = new KafkaController('localhost:2181');
-
-kafka.initializeConsumer([{ topic: 'test', partition: 0 }]);
-
-kafka.initializeProducer().then((isStarted: boolean)=> {
-    //isStarted ? kafka.sendPayload([ { topic: 'test', messages: "finally working again" , partitions: 1 }]): console.log('not started');
-    console.log(isStarted);
-    kafka.createProducerTopics(['test1', 'test2']);
-});
 
 
 
