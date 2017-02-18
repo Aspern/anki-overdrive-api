062b2570bd556e37b6a57de33d4fb825fe34b59d
Waqar Ahmed <waqar.ahmed@rwth-aachen.de>
2017-02-18 17:19:59 +0100

    added test for kafka controller

--- test/controller/kafkacontroller-test.ts
+++ test/controller/kafkacontroller-test.ts
@@ -4,24 +4,38 @@ import {VehicleScanner} from "../../src/core/vehicle/vehicle-scanner";
 import {Track} from "../../src/core/track/track-interface";
 import {AnkiOverdriveTrack} from "../../src/core/track/anki-overdrive-track";
 import {StraightPiece} from "../../src/core/track/straight-piece";
+import {KafkaController} from "../../src/controller/kafka/kafkacontroller";
+import {ConsumerMessage} from "../../src/controller/kafka/ConsumerMessage";
 
 @suite
 class KafkaControllerTest {
 
-    @test "kafka pieces"() {
-        let track: Track = AnkiOverdriveTrack.build([
-            new StraightPiece(10),
-            new StraightPiece(10),
-            new StraightPiece(10),
-            new StraightPiece(11)
-        ]);
+    static kafka: KafkaController;
 
-        expect(track.findPieces(10).length).to.equals(3);
-        expect(track.findPieces(11).length).to.equals(1);
-        expect(track.findPieces(0).length).to.equals(0);
-
-        track.findPieces(10).forEach((piece) => {
-            expect(piece.id).to.equals(10);
-        });
+    @timeout(5000)
+    static before(done: Function) {
+        KafkaControllerTest.kafka = new KafkaController('localhost:2181');
+        done();
     }
+
+    @test "kafka initialize producer and create topic"(done: Function) {
+         KafkaControllerTest.kafka.initializeProducer().then((isStarted: boolean)=> {
+             KafkaControllerTest.kafka.createProducerTopics(["test"]);
+            done();
+        }).catch((e) => done(e));
+    }
+
+    @test "kafka initialize consumer"(done: Function){
+            KafkaControllerTest.kafka.addListener((message: any) => {
+                if(message == "helloworld") done();
+            }, ConsumerMessage);
+            KafkaControllerTest.kafka.initializeConsumer([{ topic: 'test', partition: 0 }]);
+            done();
+    }
+
+    @test "kafka send message"(done: Function){
+        KafkaControllerTest.kafka.sendPayload([ { topic: 'cartest', messages: "helloworld" , partitions: 1 }]).catch((e) => done(e));
+        done();
+    }
+
 }
