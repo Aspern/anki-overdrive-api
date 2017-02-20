70826108a09b3860933942ead9d592144b432f0a
Adrian Weber <adrian@kuebler-weber.de>
2017-02-20 13:34:32 +0100

    migrated from module-util  to module-core/util

---
+++ src/core/util/file-result-handler.ts
@@ -0,0 +1,38 @@
+/// <reference path="../../../decl/jsonfile.d.ts"/>
+import * as jsonfile from "jsonfile";
+import {ResultHandler} from "./result-handler-interface";
+import {Distance} from "./result";
+
+
+class FileResultHandler implements ResultHandler {
+
+    map: {[key: string]: number} = {};
+
+    handle(result: Array<[Distance, Array<Distance>]>): void {
+        let me = this;
+
+        try {
+
+            result.forEach(result => {
+                let transitions = result[1];
+
+                transitions.forEach(transition => {
+                    let key = transition.transition
+                        .replace(/@/gi, "")
+                        .replace(/ => /gi, "");
+
+                    me.map[key] = transition.distance;
+                });
+            });
+
+            jsonfile.writeFileSync("./distances.json", me.map);
+
+        } catch (e) {
+            console.error(e);
+        }
+
+
+    }
+}
+
+export {FileResultHandler};
