723ed6000bb2c349ea38735b2bf7567288a37f33
Adrian Weber <adrian@kuebler-weber.de>
2017-02-21 15:06:24 +0100

    added anti collision scenario

---
+++ src/core/util/anki-console.ts
@@ -0,0 +1,122 @@
+import {isNullOrUndefined} from "util";
+import {Vehicle} from "../vehicle/vehicle-interface";
+
+/**
+ * TODO: Missing documentation
+ */
+class AnkiConsole {
+
+    private _commandHandler: (cmd: string, params: Array<any>, vehicle: string) => any = () => {
+    };
+
+    /**
+     *
+     * @param handler
+     * @return {AnkiConsole}
+     */
+    onCommand(handler: (cmd: string, params: Array<string>, vehicle: string) => any): AnkiConsole {
+        this._commandHandler = handler;
+        return this;
+    }
+
+    /**
+     * TODO: Missing documentation
+     *
+     * @param vehicles
+     */
+    initializePrompt(vehicles: Array<Vehicle>): void {
+        const readline = require('readline');
+
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            prompt: 'ANKI> '
+        });
+        rl.prompt();
+
+        rl.on('line', (line: string) => {
+            line = line.trim();
+            let input: string[] = line.split(' ');
+            let command = input[0];
+            let index: number = parseInt(input[1]);
+            let params = input.length > 2 ? input.splice(2, input.length - 1) : [];
+
+            if (command === "help") {
+                console.log('Available commands:\n' +
+                    'c [carIndex] - connect to car\n' +
+                    'd [carIndex] - disconnect from car\n' +
+                    's [carIndex] [speed] [acceleration] - set speed and acceleration of car\n' +
+                    'l [carIndex] [offset] - change lane to offset\n' +
+                    'o [carIndex] [offset] - initialized car with offset\n' +
+                    'u [carIndex] - u turn\n' +
+                    'p [carIndex] - ping car\n' +
+                    'b [carIndex] - battery level of car\n');
+                return;
+            }
+
+            if (index < 0 || index > vehicles.length || isNullOrUndefined(index)) {
+                console.log("car not found.");
+                return;
+            }
+
+            this._commandHandler(command, params, vehicles[index].id);
+
+
+            switch (command) {
+                case 'c':
+                    vehicles[index].connect().then(() => {
+                        console.log("car connected");
+                    });
+                    break;
+                case 's':
+                    let speed: number = parseInt(params[0]);
+                    let acceleration: number = parseInt(params[1]);
+                    speed = isNullOrUndefined(speed) ? 200 : speed;
+                    acceleration = isNullOrUndefined(acceleration) ? 50 : acceleration;
+                    try {
+                        vehicles[index].setSpeed(speed, acceleration);
+                    }
+                    catch (e) {
+                        //  TODO:validate if this is really save, what if message changes?
+                        if (e.message === "Cannot read property 'write' of undefined")
+                            console.log("error. car is not connected!");
+                    }
+                    break;
+                // TODO: Other commands are not saved from unconnected vehicles => throw error.
+                case 'o':
+                    vehicles[index].setOffset(parseFloat(params[0]));
+                    break;
+                case 'l':
+                    vehicles[index].changeLane(parseFloat(params[0]));
+                    break;
+                case 'u':
+                    vehicles[index].uTurn();
+                    break;
+                case 'd':
+                    vehicles[index].disconnect().then(() => {
+                        console.log("car disconnected");
+                    });
+                    break;
+                case 'p':
+                    vehicles[index].queryPing().then((result) => {
+                        isNullOrUndefined(result) ? console.log('no data, may be car not connected.') : console.log(result);
+                    });
+                    break;
+                case 'b':
+                    vehicles[index].queryBatteryLevel().then((result) => {
+                        isNullOrUndefined(result) ? console.log('no data, may be car not connected.') : console.log(result);
+                    });
+                    break;
+                default:
+                    console.log('command not found');
+                    break;
+            }
+            rl.prompt();
+        }).on('close', () => {
+            console.log('Good bye. Thank you for using Anki!');
+            process.exit();
+        });
+    }
+}
+
+export {AnkiConsole}
---
+++ src/script/anti-collision.ts
@@ -0,0 +1,119 @@
+import {VehicleScanner} from "../core/vehicle/vehicle-scanner";
+import {JsonSettings} from "../core/settings/json-settings";
+import {SimpleDistanceFilter} from "../core/filter/simple-distance-filter";
+import {Vehicle} from "../core/vehicle/vehicle-interface";
+import {PositionUpdateMessage} from "../core/message/position-update-message";
+import {AnkiConsole} from "../core/util/anki-console";
+import {Distance} from "../core/filter/distance";
+import {isNullOrUndefined} from "util";
+import {LightConfig} from "../core/vehicle/light-config";
+
+let scanner = new VehicleScanner(),
+    settings = new JsonSettings(),
+    ankiConsole = new AnkiConsole(),
+    track = settings.getAsTrack("track"),
+    filter = new SimpleDistanceFilter(),
+    store: {[key: string]: {speed: number, vehicle: Vehicle}} = {};
+
+function driveNormal(message: PositionUpdateMessage): void {
+    let record = store[message.vehicleId];
+
+    if (message.speed - 20 < record.speed)
+        speedUp(message);
+    else
+        record.vehicle.setLights([
+            new LightConfig()
+                .green()
+                .steady(0),
+            new LightConfig()
+                .red()
+                .steady(0),
+            new LightConfig()
+                .blue()
+                .steady()
+        ]);
+}
+
+function brake(message: PositionUpdateMessage) {
+    let record = store[message.vehicleId];
+
+    record.vehicle.setSpeed(message.speed - 50, 200);
+    record.vehicle.setLights([
+        new LightConfig()
+            .green()
+            .steady(0),
+        new LightConfig()
+            .red()
+            .steady(),
+        new LightConfig()
+            .blue()
+            .steady(0)
+    ]);
+}
+
+function speedUp(message: PositionUpdateMessage) {
+    let record = store[message.vehicleId];
+
+    record.vehicle.setSpeed(record.speed, 50);
+    record.vehicle.setLights([
+        new LightConfig()
+            .green()
+            .steady(),
+        new LightConfig()
+            .red()
+            .steady(0),
+        new LightConfig()
+            .blue()
+            .steady(0)
+    ]);
+}
+
+function handleAntiCollision(message: PositionUpdateMessage, distance: Distance): boolean {
+
+    console.log(distance.horizontal);
+
+    if (distance.horizontal <= 500) {
+        brake(message);
+    } else if (distance.horizontal > 700) {
+        driveNormal(message);
+    }
+
+    return true;
+}
+
+function supervise(message: PositionUpdateMessage): void {
+    let distances = message.distances,
+        onCollision = false;
+
+    distances.forEach(distance => {
+        if (!isNullOrUndefined(distance.delta)
+            && distance.delta < 0
+            && distance.vertical <= 34)
+            onCollision = handleAntiCollision(message, distance);
+    });
+
+    if (!onCollision)
+        driveNormal(message);
+}
+
+scanner.findAll().then(vehicles => {
+
+    vehicles.forEach(vehicle => {
+        store[vehicle.id] = {
+            speed: 0,
+            vehicle: vehicle
+        };
+    });
+
+    filter.init([track, vehicles]);
+    filter.onUpdate(supervise);
+    filter.start();
+
+    ankiConsole.onCommand((cmd, params, vehicle) => {
+        if (cmd === 's')
+            store[vehicle].speed = parseInt(params[0]);
+    }).initializePrompt(vehicles);
+}).catch(e => {
+    console.error(e);
+    process.exit();
+});
