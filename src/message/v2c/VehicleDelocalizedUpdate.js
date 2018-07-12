e310178c9953bdcde883c5e015751c7fcb3a7886
Adrian.Weber <adrian@kuebler-weber.de>
2018-07-12 18:13:05 +0200

    Remove unnecessary js files

--- src/message/v2c/VehicleDelocalizedUpdate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AbstractVehicleMessage_1 = require("../AbstractVehicleMessage");
-class VehicleDelocalizedUpdate extends AbstractVehicleMessage_1.AbstractVehicleMessage {
-    constructor(vehicleId, payload) {
-        super(vehicleId, payload);
-    }
-}
-exports.VehicleDelocalizedUpdate = VehicleDelocalizedUpdate;
