f92bf4b510efce192a9418d855be88c68e7d0bab
Adrian.Weber <adrian@kuebler-weber.de>
2018-10-10 16:30:28 +0200

    Added message name and to json string method to all messages

--- src/message/AbstractVehicleMessage.ts
+++ src/message/AbstractVehicleMessage.ts
@@ -5,11 +5,20 @@ abstract class AbstractVehicleMessage implements IVehicleMessage {
     public readonly vehicleId: string;
     public readonly timestamp: Date;
     public readonly payload: Buffer;
+    public readonly name: string
 
     protected constructor(vehicleId: string, payload: Buffer) {
         this.vehicleId = vehicleId
         this.payload = payload
         this.timestamp = new Date()
+        this.name = this.constructor.name
+    }
+
+    public toJsonString() {
+        return JSON.stringify(
+            this,
+            (key, value) => key === 'payload' ? undefined : value
+        )
     }
 }
 
